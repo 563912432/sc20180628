9392c0a566d8cfd12dab55e88871b78a2a991591
zhangkai <187212872@qq.com>
2018-11-26 10:28:02 +0800

    tz

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -7,7 +7,7 @@ module.exports = {
       setImages (data) {
         var imagesData = []
         data.forEach(function (v, i) {
-          imagesData.push({'src': 'http://www.wdexam.com/' + '/Uploads/' + v.thumb, w: v.width, h: v.height, 'title': v.title, id: v.id})
+          imagesData.push({'src': 'http://www.wenyunjy.com/' + '/Uploads/' + v.thumb, w: v.width, h: v.height, 'title': v.title, id: v.id})
         })
         this.$store.commit('setImages', imagesData)
       }
